9028e67c0643e28d61d8298b1bdf28e81a856a0e
Ali Sajjad <alisajjad1272>
2021-09-28 19:31:35 +0500

    Basic User Auth

---
+++ ror/app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+
+  before_save :lowercase_email
+
+  def lowercase_email
+    self.email = email.downcase
+  end
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/, message: 'Invalid email' }
+  validates :password_digest, presence: true, length: { minimum: 4 }
+  validates :first_name, presence: true, length: { maximum: 35, minimum: 1}
+  validates :last_name, presence: true, length: { maximum: 35, minimum: 1}
+
+  has_secure_password
+
+end
--- ror/config/routes.rb
+++ ror/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sign-in', as: 'sign_in', to: 'auth#sign_in'
+  post 'sign-in', as: 'login', to: 'auth#login'
+  get 'logout', as: 'logout', to: 'auth#logout'
+
+  get 'create-test-user', as: 'test-sign-in', to: 'auth#test_create_user'
+  get 'users', as: 'users', to: 'auth#users'
+
+  root 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
